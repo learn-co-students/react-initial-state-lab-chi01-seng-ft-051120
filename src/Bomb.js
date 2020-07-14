4fd2f88c1a110fcd20e752a1412edc1e4d89f1ca
github username <github email address>
2020-07-14 12:46:35 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,25 @@
-// your Bomb code here!
+import React from 'react';
+
+class Bomb extends React.Component {
+
+  constructor(props) {
+    super()
+
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+  secondsLeft = () => {
+    this.setState({
+      currentSlideIndex: this.state.secondsLeft
+    })
+  }
+
+  render() {
+    const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
+
+      return (
+        <div>{message}</div>
+      )
+    }
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,27 @@
+import { render } from "enzyme"
+
 // your ImageSlider code here!
+class ImageSlider extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
+
+  render() {
+    return (
+      <h1>
+        {this.state.currentSlideIndex}
+        </h1>
+      )
+    }
+}
+
+export default ImageSlider;
+
+
+
+//Its initial state should have a property called currentSlideIndex that starts at 0.
+//It should only render out the text 'I am on slide <CURRENT_SLIDE>',
+// where <CURRENT_SLIDE> is the value of this.state.currentSlideIndex.
